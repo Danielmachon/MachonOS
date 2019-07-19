443e7f30deca22ffc40539b8b86a184b0751914f
Danielmachon <dmachon.dev@gmail.com>
2019-07-19 15:09:51 +0200

    tmp

--- arch/x86/pic.c
+++ arch/x86/pic.c
@@ -3,10 +3,10 @@
 
 #define PIC1            0x20		    /* IO base address for master PIC */
 #define PIC2            0xA0		    /* IO base address for slave PIC */
-#define PIC1_COMMAND    PIC1
-#define PIC1_DATA       (PIC1+1)
-#define PIC2_COMMAND    PIC2
-#define PIC2_DATA       (PIC2+1)
+#define PIC1_COMMAND    PIC1            /* Master command port */
+#define PIC1_DATA       (PIC1+1)        /* Master data port */
+#define PIC2_COMMAND    PIC2            /* Slave command port */
+#define PIC2_DATA       (PIC2+1)        /* Slave data port */
 #define PIC_EOI         0x20            /* End of interrupt */
 
 #define ICW1_ICW4	    0x01            /* ICW4 (not) needed */
